15b5bfc1c07c516daa176eae335210ac0a3d4723
samuelg4133 <leosgv.sv@gmail.com>
2021-08-25 21:13:08 -0300

    done

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
-export function Content() {
-  // Complete aqui
+import React from "react";
+
+import "../styles/content.scss";
+
+interface ContentProps {
+  title: string;
 }
+
+export const Content: React.FC<ContentProps> = ({ title, children }) => {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">{children}</div>
+      </main>
+    </div>
+  );
+};
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,14 @@
-export function SideBar() {
-  // Complete aqui
-}
+import React from "react";
+import "../styles/sidebar.scss";
+
+export const SideBar: React.FC = ({ children }) => {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">{children}</div>
+    </nav>
+  );
+};
